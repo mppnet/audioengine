bfdc7491f6df859529bfdddb149c569f4e218e0f
sophie <sofija@duck.com>
2024-12-28 23:21:50 +0200

    feat: lots of small fixes + example video

--- audioengine.cpp
+++ audioengine.cpp
@@ -7,77 +7,69 @@
 #define TSF_IMPLEMENTATION
 #include "tsf/tsf.h"
 
-namespace emscrijpten {
-    typedef std::function<void()> callback_t;
-    void async_call_callback(void * arg) {
-        callback_t * callback = (callback_t *)arg;
-        (*callback)();
-        delete callback;
-    }
-    void async_call(callback_t callback, int millis = 0) {
-        callback_t * _callback = new callback_t(callback);
-        emscripten_async_call(async_call_callback, _callback, millis);
-    }
-
-}
-
-static tsf* g_TinySoundFont;
-
+static tsf *g_TinySoundFont = NULL;
 
 static bool run;
-static int volume = 100;
+int volume = 100;
 
 static bool renderAudio(int numInputs, const AudioSampleFrame *inputs,
-                      int numOutputs, AudioSampleFrame *outputs,
-                      int numParams, const AudioParamFrame *params,
-                      void *userData)
+                        int numOutputs, AudioSampleFrame *outputs,
+                        int numParams, const AudioParamFrame *params,
+                        void *userData)
 {
-	for(int i = 0; i < numOutputs; ++i) {
-		tsf_render_float(g_TinySoundFont, outputs[i].data, outputs[i].samplesPerChannel*outputs[i].numberOfChannels, 0);
-	}
+  for (int i = 0; i < numOutputs; ++i)
+  {
+    tsf_render_float(g_TinySoundFont, outputs[i].data, outputs[i].samplesPerChannel * outputs[i].numberOfChannels, 0);
+  }
 
-	return true;
+  return true;
 }
 
-void load(uintptr_t data, int length) {
-    printf("hello world %d\n", length);
-    auto array = reinterpret_cast<uint8_t *>( data );
-  
-    g_TinySoundFont = tsf_load_memory(array, length);
+void load(uintptr_t data, int length)
+{
+  printf("A file of length %d has been loaded into AudioEngine.\n", length);
+  auto array = reinterpret_cast<uint8_t *>(data);
+  bool recreating = false;
+  if (g_TinySoundFont != NULL)
+  {
+    tsf_close(g_TinySoundFont);
+    recreating = true;
+  }
+  g_TinySoundFont = tsf_load_memory(array, length);
+  if (recreating)
+  {
+    tsf_set_output(g_TinySoundFont, TSF_MONO, 44100, 0);
+    tsf_set_max_voices(g_TinySoundFont, 200);
+    tsf_set_volume(g_TinySoundFont, volume / 100);
+  }
 }
 
-void destroy() {
-	tsf_close(g_TinySoundFont);
+void destroy()
+{
+  tsf_close(g_TinySoundFont);
 }
 
-void end() {
-	run = false;
+void end()
+{
+  run = false;
 }
-void note_on(int presetId, int key, float velocity, int delay) {
+void note_on(int presetId, int key, float velocity, int delay)
+{
   tsf_note_on(g_TinySoundFont, presetId, key, velocity);
 }
 
-void note_off(int presetId, int key, float velocity, int delay) {
+void note_off(int presetId, int key, float velocity, int delay)
+{
   tsf_note_off(g_TinySoundFont, presetId, key);
-}
-
-void set_volume(int vol) {
-  // nulled out
-}
-
-int get_volume() {
-  // always 100
-	return 100;
 }
 
 uint8_t audioThreadStack[4096];
 
 bool OnClick(int eventType, const EmscriptenMouseEvent *mouseEvent, void *userData)
 {
-  printf("canvas clicked\n");
-
   EMSCRIPTEN_WEBAUDIO_T audioContext = (EMSCRIPTEN_WEBAUDIO_T)userData;
-  if (emscripten_audio_context_state(audioContext) != AUDIO_CONTEXT_STATE_RUNNING) {
+  if (emscripten_audio_context_state(audioContext) != AUDIO_CONTEXT_STATE_RUNNING)
+  {
     emscripten_resume_audio_context_sync(audioContext);
   }
   return false;
@@ -85,56 +77,68 @@ bool OnClick(int eventType, const EmscriptenMouseEvent *mouseEvent, void *userDa
 
 void AudioWorkletProcessorCreated(EMSCRIPTEN_WEBAUDIO_T audioContext, bool success, void *userData)
 {
-  if (!success) return; // Check browser console in a debug build for detailed errors
-  printf("pressor created\n");
+  if (!success)
+    return; // Check browser console in a debug build for detailed errors
 
-  int outputChannelCounts[1] = { 1 };
+  int outputChannelCounts[1] = {1};
   EmscriptenAudioWorkletNodeCreateOptions options = {
-    .numberOfInputs = 0,
-    .numberOfOutputs = 1,
-    .outputChannelCounts = outputChannelCounts
-  };
+      .numberOfInputs = 0,
+      .numberOfOutputs = 1,
+      .outputChannelCounts = outputChannelCounts};
 
   // Create node
   EMSCRIPTEN_AUDIO_WORKLET_NODE_T wasmAudioWorklet = emscripten_create_wasm_audio_worklet_node(audioContext,
-                                                            "noise-generator", &options, &renderAudio, 0);
+                                                                                               "audioengine", &options, &renderAudio, 0);
 
   // Connect it to audio context destination
   emscripten_audio_node_connect(wasmAudioWorklet, audioContext, 0, 0);
-  emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, (void*)audioContext, 0, OnClick);
+  emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, (void *)audioContext, 0, OnClick);
 }
 
 void AudioThreadInitialized(EMSCRIPTEN_WEBAUDIO_T audioContext, bool success, void *userData)
 {
-  if (!success) return; // Check browser console in a debug build for detailed errors
-  printf("thread initialized\n");
+  if (!success)
+    return; // Check browser console in a debug build for detailed errors
 
   WebAudioWorkletProcessorCreateOptions opts = {
-    .name = "noise-generator",
+      .name = "audioengine",
   };
   emscripten_create_wasm_audio_worklet_processor_async(audioContext, &opts, &AudioWorkletProcessorCreated, 0);
 }
 
-int start()
+int get_volume()
 {
-    printf("we started\n");
-    
-    tsf_set_output(g_TinySoundFont, TSF_STEREO_INTERLEAVED, 22000, 0);
-    tsf_set_max_voices(g_TinySoundFont, 40);
-    EMSCRIPTEN_WEBAUDIO_T context = emscripten_create_audio_context(0);
-    emscripten_start_wasm_audio_worklet_thread_async(context, audioThreadStack, sizeof(audioThreadStack),
-                                                   &AudioThreadInitialized, 0);
-												   
-	return 0;
+  return volume;
 }
 
-EMSCRIPTEN_BINDINGS(my_module) {
-    emscripten::function("start", start);
-    emscripten::function("load", load, emscripten::allow_raw_pointers());
-    emscripten::function("end", end);
-    emscripten::function("destroy", destroy);
-    emscripten::function("note_off", note_off);
-    emscripten::function("note_on", note_on);
-	emscripten::function("set_volume", set_volume);
-	emscripten::function("get_volume", get_volume);
+void set_volume(int vol)
+{
+  volume = vol;
+  tsf_set_volume(g_TinySoundFont, vol / 100);
+}
+
+int start()
+{
+  printf("AudioEngine has been started.\n");
+
+  tsf_set_output(g_TinySoundFont, TSF_MONO, 44100, 0);
+  tsf_set_max_voices(g_TinySoundFont, 200);
+  EmscriptenWebAudioCreateAttributes attr = {
+      .latencyHint = "playback",
+      .sampleRate = 44100};
+  EMSCRIPTEN_WEBAUDIO_T context = emscripten_create_audio_context(&attr);
+  emscripten_start_wasm_audio_worklet_thread_async(context, audioThreadStack, sizeof(audioThreadStack), &AudioThreadInitialized, 0);
+  return 0;
+}
+
+EMSCRIPTEN_BINDINGS(my_module)
+{
+  emscripten::function("start", start);
+  emscripten::function("load", load, emscripten::allow_raw_pointers());
+  emscripten::function("end", end);
+  emscripten::function("destroy", destroy);
+  emscripten::function("note_off", note_off);
+  emscripten::function("note_on", note_on);
+  emscripten::function("set_volume", set_volume);
+  emscripten::function("get_volume", get_volume);
 }
--- readme.md
+++ readme.md
@@ -2,21 +2,25 @@
 
 this is a c++ (compiled to wasm) application which is a supposed replacement for the current AudioEngineWeb that's in multiplayerpiano.  
 
-it can reliably play most black midis and normal midis with resonable performance with absolutely no cracking or artifacting.
+it can reliably play most black midis and normal midis with resonable performance with absolutely no  artifacting.
 
-this is very heavily in development still, and is not a final version at all.
-
-we are mainly missing all Synth stuff, and audio playback is only mono.
+this is very heavily in development still, and is not a final version at all. 
 
 uses [TinySoundFont](https://github.com/schellingb/TinySoundFont) and [emscripten](https://emscripten.org).
 
+[Video - freedom dive black midi](https://files.sad.ovh/public/2024-12-28%2017-40-26.mp4)
+
+## bugs
+1. The only major bug is the fact that it's still mono! There's a ongoing TSF issue, which I've reported is still happening.
+2. Also, higher than 100-ish voice count crackles (we're using 200 voices for better blackmidi playback).. I have no clue how to fix this, maybe it's just a issue with how small the sample size for webaudio is (128 samples??).
+3. Volume control is broken. Created a issue for this on the TSF repo.
 
 ## build
 use (with emsdk activated and sourced)
 ```
 mkdir -p build
 
-emcc -std=c++11 -lembind -s ALLOW_MEMORY_GROWTH -s EXPORTED_FUNCTIONS="['_malloc', '_free']" -s AUDIO_WORKLET=1 -s WASM_WORKERS=1 -O3 -o build/audioengine.js audioengine.cpp 
+emcc -std=c++11 -lembind -s ALLOW_MEMORY_GROWTH -s EXPORTED_FUNCTIONS="['_malloc', '_free']" -s AUDIO_WORKLET=1 -s WASM_WORKERS=1 -O3 --emit-tsd audioengine.ts.d -o build/audioengine.js audioengine.cpp
 ```
 
 to compile into `build`
